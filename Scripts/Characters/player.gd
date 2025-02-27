70f851178ba6889cfbaef8693da3f4dfba0b302a
Kiernen Dabbs <kiernen.v.dabbs@gmail.com>
2025-02-26 23:34:54 -0600

    complete player movement and animation

--- Scripts/Characters/player.gd
+++ Scripts/Characters/player.gd
@@ -1,26 +1,50 @@
 extends CharacterBody2D
 
-@export var SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
+@export var SPEED = 4
+@export var gravity = 30
+@export var JUMP_STRENGTH = 6
+
+@onready var ap = $AnimationPlayer
+@onready var sprite = $Sprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if !is_on_floor():
+		velocity.y += gravity
+		if velocity.y > 1000:
+			velocity.y = 1000
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = -JUMP_STRENGTH * 100
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var horizontal_direction := Input.get_axis("ui_left", "ui_right")
+	velocity.x = horizontal_direction * SPEED * 100
+
+	if horizontal_direction != 0:
+		switch_direction(horizontal_direction)
 
 	move_and_slide()
+	
+	update_animations(horizontal_direction)
+	
+func update_animations(horizontal_direction):
+	if is_on_floor():
+		if horizontal_direction == 0:
+			ap.play("idle")
+		else:
+			ap.play("run")
+	else:
+		if velocity.y < 0:
+			ap.play("jump")
+		elif velocity.y > 0:
+			ap.play("fall")
+			
+func switch_direction(horizontal_directon):
+	sprite.flip_h = (horizontal_directon == -1)
+	sprite.position.x = horizontal_directon * 4
 	
 	#extends CharacterBody2D
 #
